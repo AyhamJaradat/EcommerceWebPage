302b312fbd60713ba4136888d43f8a562f12fe6a
Ayham Jaradat <a_snone@live.com>
2022-10-07 00:21:02 +0300

    Final touches

--- files/js/main.js
+++ files/js/main.js
@@ -1,32 +1,34 @@
-
-function scrollHorizontally( isRight){
-    if(isRight){
-        document.getElementById("horizontal-scrollable").scrollLeft += 50;
-    }else{
-        document.getElementById("horizontal-scrollable").scrollLeft -= 50;
-    }
-    
+/**
+ * Scroll the products list right or left
+ * @param isRight boolean
+ */
+function scrollHorizontally(isRight) {
+  if (isRight) {
+    document.getElementById("horizontal-scrollable").scrollLeft += 50;
+  } else {
+    document.getElementById("horizontal-scrollable").scrollLeft -= 50;
+  }
 }
+/**
+ * Show or Hide scrolling buttons when scrolled to an end
+ */
 function onHorizontalScroll() {
-    const scrollable = document.getElementById("horizontal-scrollable");
-    const previousButton = document.getElementById("previousBtn");
-    const nextButton = document.getElementById("nextBtn");
-    console.log("scrollLeft", scrollable.scrollLeft);
-    console.log("scrollWidth", scrollable.scrollWidth );
-    console.log("clientWidth", scrollable.clientWidth);
-    console.log("reachEnd? ", scrollable.scrollWidth - scrollable.scrollLeft);
-    console.log("reachEnd? ", scrollable.scrollWidth - scrollable.scrollLeft === scrollable.clientWidth );
+  const scrollable = document.getElementById("horizontal-scrollable");
+  const previousButton = document.getElementById("previousBtn");
+  const nextButton = document.getElementById("nextBtn");
 
+  if (scrollable.scrollLeft <= 0) {
+    previousButton.classList.add("hidden");
+  } else {
+    previousButton.classList.remove("hidden");
+  }
 
-    if (scrollable.scrollLeft <= 0){
-        previousButton.classList.add("hidden");
-    }else {
-        previousButton.classList.remove("hidden");
-    }
-
-    if(scrollable.scrollWidth - scrollable.scrollLeft <= scrollable.clientWidth){
-        nextButton.classList.add("hidden");
-    }else {
-        nextButton.classList.remove("hidden");
-    }
+  if (
+    scrollable.scrollWidth - scrollable.scrollLeft <=
+    scrollable.clientWidth
+  ) {
+    nextButton.classList.add("hidden");
+  } else {
+    nextButton.classList.remove("hidden");
+  }
 }
