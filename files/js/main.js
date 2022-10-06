cd0a9c6d656cb9d0174fcc0c1aa6bc053b009f5b
Ayham Jaradat <a_snone@live.com>
2022-10-06 15:11:40 +0300

    WIP

---
+++ files/js/main.js
@@ -0,0 +1,46 @@
+function initializePageEvents() {
+//   const nextButton = document.getElementById("nextBtn");
+//   nextButton.onclick = () => {
+//     console.log("Clciked")
+//     document.getElementById("horizontal-scrollable").scrollLeft += 20;
+//   };
+
+//   const previousButton = document.getElementById("previousBtn");
+//   previousButton.onclick = () => {
+//     console.log("Clciked")
+//     document.getElementById("horizontal-scrollable").scrollLeft -= 20;
+//   };
+
+}
+
+function scrollHorizontally( isRight){
+    if(isRight){
+        document.getElementById("horizontal-scrollable").scrollLeft += 20;
+    }else{
+        document.getElementById("horizontal-scrollable").scrollLeft -= 20;
+    }
+    
+}
+function onHorizontalScroll() {
+    const scrollable = document.getElementById("horizontal-scrollable");
+    const previousButton = document.getElementById("previousBtn");
+    const nextButton = document.getElementById("nextBtn");
+    console.log("scrollLeft", scrollable.scrollLeft);
+    console.log("scrollWidth", scrollable.scrollWidth );
+    console.log("clientWidth", scrollable.clientWidth);
+    console.log("reachEnd? ", scrollable.scrollWidth - scrollable.scrollLeft);
+    console.log("reachEnd? ", scrollable.scrollWidth - scrollable.scrollLeft === scrollable.clientWidth );
+
+
+    if (scrollable.scrollLeft <= 0){
+        previousButton.classList.add("hidden");
+    }else {
+        previousButton.classList.remove("hidden");
+    }
+
+    if(scrollable.scrollWidth - scrollable.scrollLeft <= scrollable.clientWidth){
+        nextButton.classList.add("hidden");
+    }else {
+        nextButton.classList.remove("hidden");
+    }
+}
